fdb8763f5d4514d1be7d958508304a1467fa666d
Michel Zam <67806901+michelzam@users.noreply.github.com>
2023-01-28 11:26:22 +0100

    updated on 2023-01-28 11:26:21.879620

--- main.py
+++ main.py
@@ -10,14 +10,14 @@
 ◀️💡▶️
 1. What's **new**?  :eight_pointed_black_star:  
 1. Hi ... 
-1. ===Almost=== *nothing*
+1. ***Almost*** *nothing*
 
 | Syntax      | Description |
 | ----------- | ----------- |
 | Header      | Title       |
 | Paragraph   | Text        |
    
-    
+     
 ---
 """
  
