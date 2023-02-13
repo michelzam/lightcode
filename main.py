1a9cec54607683ea201c45e88fc92d08fd1f78e4
Michel Zam <67806901+michelzam@users.noreply.github.com>
2023-02-13 13:42:30 +0100

    updated on 2023-02-13 13:42:30.262633

--- main.py
+++ main.py
@@ -2,21 +2,7 @@
 ## :smile: Hi ==there== !
 ◀️☀️️▶️
 
-[UWM Trello](https://trello.com/b/azfzdTGS/uwm)
-
-:arrow_backward::sparkles::arrow_forward:
-
-
-◀️💡▶️
-1. What's **new**?  :eight_pointed_black_star:  
-1. Hi ... 
-1. `Almost` *nothing*
- 
-| Syntax      | Description |
-| ----------- | ----------- |
-| Header      | :blue[Title] |
-| Paragraph   | `Text'        |
-   
+Simple script, Murach style
  
 ---
 """
