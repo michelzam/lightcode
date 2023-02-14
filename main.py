99835a94088f8a198ae37a897ecc5c4111a8fac4
Michel Zam <67806901+michelzam@users.noreply.github.com>
2023-02-14 03:47:15 +0100

    updated on 2023-02-14 03:47:15.237496

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 """
-# Student loan calculator
-M = L[i(1+i)n] / [(1+i)n-1]
-- M = monthly payment
-- L = Loan amount
-- i = interest rate (for an interest rate of 5%, i = 0.05)
-- n = number of payments
-"""
+# Simple script
+## Murach style
 
-#Declare variables
-monthly_payment = 0 
-loan_amount = 0
-interest_rate = 0
-number_of_payments = 0
-loanduration_in_years = 0
-#payments
-loan_amount = input("How much money will you borrow? ")
+""" 
+ 
+import math
+import pandas
 
-interest_rate = input("What is the interest rate on the loan? ")
 
-loanduration_in_years = input("How many years will it take you to pay off the loan? ")
+def hello(msg: str) -> str:
+    return str
 
-#Convert the strings into floating numbers so we can use them in the  formula
-loanduration_in_years = float(loanduration_in_years)
-loan_amount = float(loan_amount)
-interest_rate = float(interest_rate)
+print("in")
+mpg = float(input("Input value here:"))
 
-#Since payments are once per month, number of payments is number of years for the loan
+x = hello("world")
 
-number_of_payments = loanduration_in_years * 12
-
-#calculate the monthly payment based on the formula
-
-monthly_payment = loan_amount * interest_rate * (1 + interest_rate) * number_of_payments / ((1 + interest_rate) * number_of_payments - 1)
-
-#Result to the program
-
-print("Your monthly payment will be " + str(monthly_payment))
+a = 0 
+b = 1
+c = (a + b) + mpg 
+d = c
+e = d + a 
